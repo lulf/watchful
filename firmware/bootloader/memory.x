63060bd57dd7efaf4723888b54945cf604341937
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2023-07-15 22:47:44 +0200

    ifix: add bootloader

--- firmware/bootloader/memory.x
+++ firmware/bootloader/memory.x
@@ -3,16 +3,30 @@ MEMORY
   /* NOTE 1 K = 1 KiBi = 1024 bytes */
   MBR                               : ORIGIN = 0x00000000, LENGTH = 4K
   SOFTDEVICE                        : ORIGIN = 0x00001000, LENGTH = 152K
-  FLASH                             : ORIGIN = 0x00026000, LENGTH = 160K
+  ACTIVE                            : ORIGIN = 0x00026000, LENGTH = 160K
   DFU                               : ORIGIN = 0x0004E000, LENGTH = 164K
-  BOOTLOADER                        : ORIGIN = 0x00077000, LENGTH = 24K
+  FLASH                             : ORIGIN = 0x00077000, LENGTH = 24K
   BOOTLOADER_STATE                  : ORIGIN = 0x0007D000, LENGTH = 4K
   STORAGE                           : ORIGIN = 0x0007E000, LENGTH = 4k
-  RAM                               : ORIGIN = 0x2000BAF0, LENGTH = 17680
+  RAM                         (rwx) : ORIGIN = 0x20000008, LENGTH = 0xfff8
+  uicr_bootloader_start_address (r) : ORIGIN = 0x10001014, LENGTH = 0x4
 }
 
 __bootloader_state_start = ORIGIN(BOOTLOADER_STATE);
 __bootloader_state_end = ORIGIN(BOOTLOADER_STATE) + LENGTH(BOOTLOADER_STATE);
 
+__bootloader_active_start = ORIGIN(ACTIVE);
+__bootloader_active_end = ORIGIN(ACTIVE) + LENGTH(ACTIVE);
+
 __bootloader_dfu_start = ORIGIN(DFU);
 __bootloader_dfu_end = ORIGIN(DFU) + LENGTH(DFU);
+
+__bootloader_start = ORIGIN(FLASH);
+
+SECTIONS
+{
+  .uicr_bootloader_start_address :
+  {
+    LONG(__bootloader_start)
+  } > uicr_bootloader_start_address
+}
---
+++ firmware/bootloader/src/main.rs
@@ -0,0 +1,62 @@
+#![no_std]
+#![no_main]
+
+use core::cell::RefCell;
+
+use cortex_m_rt::{entry, exception};
+
+#[cfg(feature = "defmt")]
+use defmt_rtt as _;
+
+use embassy_boot_nrf::*;
+use embassy_nrf::nvmc::Nvmc;
+use embassy_nrf::wdt;
+use embassy_sync::blocking_mutex::Mutex;
+
+#[entry]
+fn main() -> ! {
+    let p = embassy_nrf::init(Default::default());
+
+    // Uncomment this if you are debugging the bootloader with debugger/RTT attached,
+    // as it prevents a hard fault when accessing flash 'too early' after boot.
+    /*
+        for i in 0..10000000 {
+            cortex_m::asm::nop();
+        }
+    */
+
+    let mut wdt_config = wdt::Config::default();
+    wdt_config.timeout_ticks = 32768 * 5; // timeout seconds
+    wdt_config.run_during_sleep = true;
+    wdt_config.run_during_debug_halt = false;
+
+    let flash = WatchdogFlash::start(Nvmc::new(p.NVMC), p.WDT, wdt_config);
+    let flash = Mutex::new(RefCell::new(flash));
+
+    let config = BootLoaderConfig::from_linkerfile_blocking(&flash);
+    let active_offset = config.active.offset();
+    let mut bl: BootLoader<_, _, _> = BootLoader::new(config);
+
+    bl.prepare();
+
+    unsafe { bl.load(active_offset) }
+}
+
+#[no_mangle]
+#[cfg_attr(target_os = "none", link_section = ".HardFault.user")]
+unsafe extern "C" fn HardFault() {
+    cortex_m::peripheral::SCB::sys_reset();
+}
+
+#[exception]
+unsafe fn DefaultHandler(_: i16) -> ! {
+    const SCB_ICSR: *const u32 = 0xE000_ED04 as *const u32;
+    let irqn = core::ptr::read_volatile(SCB_ICSR) as u8 as i16 - 16;
+
+    panic!("DefaultHandler #{:?}", irqn);
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    cortex_m::asm::udf();
+}
