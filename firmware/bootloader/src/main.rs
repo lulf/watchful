1262a52c6c162152cc60c120ff36fb570403150a
Ulf Lilleengen <lulf@redhat.com>
2023-08-11 22:47:25 +0200

    make it work with regular spi device

--- firmware/bootloader/src/main.rs
+++ firmware/bootloader/src/main.rs
@@ -8,7 +8,7 @@ use defmt::info;
 use defmt_rtt as _;
 use embassy_boot_nrf::*;
 use embassy_embedded_hal::flash::partition::BlockingPartition;
-use embassy_embedded_hal::shared_bus::blocking::spi::{ManagedSpiDevice, SpiDeviceWithConfig};
+use embassy_embedded_hal::shared_bus::blocking::spi::{SpiDevice, SpiDeviceWithConfig};
 use embassy_nrf::gpio::{Input, Level, Output, OutputDrive, Pull};
 use embassy_nrf::interrupt::Priority;
 use embassy_nrf::nvmc::Nvmc;
@@ -24,7 +24,7 @@ bind_interrupts!(struct Irqs {
     SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0 => spim::InterruptHandler<peripherals::TWISPI0>;
 });
 
-type ExternalFlash<'a> = XtFlash<ManagedSpiDevice<spim::Spim<'a, TWISPI0>, Output<'a, P0_05>>>;
+type ExternalFlash<'a, 'b> = XtFlash<SpiDevice<'a, NoopRawMutex, spim::Spim<'b, TWISPI0>, Output<'b, P0_05>>>;
 
 #[entry]
 fn main() -> ! {
@@ -48,32 +48,32 @@ fn main() -> ! {
     default_config.frequency = spim::Frequency::M8;
     default_config.mode = spim::MODE_3;
 
-    let spim = spim::Spim::new(p.TWISPI0, Irqs, p.P0_02, p.P0_04, p.P0_03, default_config);
+    let mut active_offset = 0;
+    let bl: BootLoader = {
+        let spim = spim::Spim::new(p.TWISPI0, Irqs, p.P0_02, p.P0_04, p.P0_03, default_config);
+        let bus = Mutex::new(RefCell::new(spim));
+        let flash_cs = Output::new(p.P0_05, Level::High, OutputDrive::Standard);
+        let flash_spi = SpiDevice::new(&bus, flash_cs);
+        let external_flash = XtFlash::new(flash_spi);
+        let external_flash = external_flash.unwrap();
+        let external_flash = Mutex::new(RefCell::new(external_flash));
 
-    let flash_cs = Output::new(p.P0_05, Level::High, OutputDrive::Standard);
-    let flash_spi = ManagedSpiDevice::new(spim, flash_cs);
-    let external_flash = XtFlash::new(flash_spi);
-    let external_flash = external_flash.unwrap();
-    let external_flash = Mutex::new(RefCell::new(external_flash));
+        let config = create_flash_config(&internal_flash, &external_flash);
+        active_offset = config.active.offset();
 
-    let config = create_flash_config(&internal_flash, &external_flash);
-    let active_offset = config.active.offset();
-
-    let mut bl: BootLoader = BootLoader::prepare(config);
-
-    drop(internal_flash);
-    drop(external_flash);
+        BootLoader::prepare(config)
+    };
 
     unsafe { bl.load(active_offset) }
 }
 
-pub fn create_flash_config<'a, 'b>(
+pub fn create_flash_config<'a, 'b, 'c>(
     internal: &'a Mutex<NoopRawMutex, RefCell<WatchdogFlash<Nvmc<'b>>>>,
-    external: &'a Mutex<NoopRawMutex, RefCell<ExternalFlash<'b>>>,
+    external: &'a Mutex<NoopRawMutex, RefCell<ExternalFlash<'b, 'c>>>,
 ) -> BootLoaderConfig<
     BlockingPartition<'a, NoopRawMutex, WatchdogFlash<Nvmc<'b>>>,
-    BlockingPartition<'a, NoopRawMutex, ExternalFlash<'b>>,
-    BlockingPartition<'a, NoopRawMutex, ExternalFlash<'b>>,
+    BlockingPartition<'a, NoopRawMutex, ExternalFlash<'b, 'c>>,
+    BlockingPartition<'a, NoopRawMutex, ExternalFlash<'b, 'c>>,
 > {
     extern "C" {
         static __bootloader_state_start: u32;
