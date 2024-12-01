3743e94230b591f89687872f2f2dc11a8878493d
Ulf Lilleengen <ulf@digili.no>
2024-12-01 09:06:50 +0100

    Fixup imgtool

--- scripts/prepare_recovery.sh
+++ scripts/prepare_recovery.sh
@@ -5,7 +5,7 @@ pushd infinitime-recovery/app && cargo objcopy --release -- -O ihex ../../app-re
 
 mergehex -m boot-recovery.hex app-recovery.hex -o infinitime-recovery.bin
 
-imgtool create --align 4 --version 1.0.0 --header-size 32 --slot-size 475136 --pad-header infinitime-recovery.bin infinitime-recovery-image.bin
+tools/mcuboot/imgtool.py create --align 4 --version 1.0.0 --header-size 32 --slot-size 475136 --pad-header infinitime-recovery.bin infinitime-recovery-image.bin
 
 #probe-rs erase --chip nRF52832_xxAA
 #probe-rs download blinky-image.bin --base-address 0x8000 --binary-format Binary --chip nRF52832_xxAA
