75c2ba6389804d64cb778daf1488cdb39210e023
Ulf Lilleengen <ulf@digili.no>
2024-12-01 08:17:33 +0100

    Add script to build recovery app

---
+++ scripts/prepare_recovery.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+#pushd infinitime-recovery/boot && cargo objcopy --release -- -O ihex ../../boot-recovery.hex && popd
+#pushd infinitime-recovery/app && cargo objcopy --release -- -O ihex ../../app-recovery.hex && popd
+
+pushd blinky && cargo objcopy --release -- -O binary ../blinky.bin && popd
+
+#mergehex -m boot-recovery.hex app-recovery.hex -o infinitime-recovery.bin
+
+imgtool create --align 4 --version 1.0.0 --header-size 32 --slot-size 475136 --pad-header blinky.bin blinky-image.bin
+
+probe-rs erase --chip nRF52832_xxAA
+probe-rs download blinky-image.bin --base-address 0x8000 --binary-format Binary --chip nRF52832_xxAA
+probe-rs download bootloader-1.0.1.bin --base-address 0 --binary-format Binary --chip nRF52832_xxAA
