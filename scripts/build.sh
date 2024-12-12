b2b1bfec76c89579f76373a77901880029133176
Ulf Lilleengen <ulf@digili.no>
2024-12-12 13:26:55 +0100

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+set -e
+pushd firmware
+cargo clean
 cargo build --release
-cargo objcopy --release -- -O ihex app.hex
-nrfutil pkg generate --debug-mode --application app.hex app.zip
+cargo objcopy --release -- -O binary watchful.bin
+popd
+./tools/mcuboot/imgtool.py create --align 4 --version 1.0.0 --header-size 32 --slot-size 475136 --pad-header ./firmware/watchful.bin watchful-image.bin
+adafruit-nrfutil dfu genpkg --dev-type 0x0052 --application watchful-image.bin watchful-dfu.zip
