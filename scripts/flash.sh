b6424d97ceea0807600e84511379bf694b78f3fa
Ulf Lilleengen <ulf@digili.no>
2025-02-09 10:19:10 +0100

    fix: update scripts

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -1,1 +1,7 @@
-nrfutil dfu ble -pkg app.zip -ic NRF52 -n "Pinetime Embassy" 
+#!/usr/bin/env bash
+set -e
+
+probe-rs erase --chip nRF52832_xxAA
+#probe-rs download pinetime-mcuboot-app-image-1.13.0.bin --format Binary --base-address 0x8000 --chip nRF52832_xxAA
+probe-rs download watchful-image.bin --binary-format Binary --base-address 0x8000 --chip nRF52832_xxAA
+probe-rs download ./tools/mcuboot/mcuboot.bin --binary-format Binary --chip nRF52832_xxAA
--- scripts/flash_infinitime.sh
+++ scripts/flash_infinitime.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
 probe-rs erase --chip nRF52832_xxAA
 #probe-rs download pinetime-mcuboot-app-image-1.13.0.bin --format Binary --base-address 0x8000 --chip nRF52832_xxAA
 probe-rs download pinetime-mcuboot-app-image-1.15.0.bin --binary-format Binary --base-address 0x8000 --chip nRF52832_xxAA
